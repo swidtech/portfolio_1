2bb13cafbf65f27a71a3136d90b38eb42650a39b
swidtech <swidtech1@gmail.com>
2024-06-03 07:24:06 +0100

    js addede

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["","Frontend Developer","Office/Information Manager","Personal/Virtual Assistant","Secretary","Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
